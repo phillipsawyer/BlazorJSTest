5607e25f285a1323cf8ad7c9cec96f6755d2c2fc
Phillip Sawyer <30532122+Azer7@users.noreply.github.com>
2020-06-04 15:56:32 -0600

    Initial Commit

---
+++ BlazorAppOfflineTest/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/main.js
@@ -0,0 +1,38 @@
+﻿function attachHandlers() {
+    document.getElementById("button").addEventListener("click", function () { alert("This works offline."); });
+
+    var nameElement = document.getElementById("name");
+    nameElement.addEventListener("keyup", function(e) {
+        if (nameElement.value.indexOf("p") > -1) {
+            document.getElementById("main-form").style.background = "red";
+        }
+    });
+
+    nameElement.addEventListener("keyup", function (e) {
+        if (nameElement.value.indexOf("p") == -1) {
+            document.getElementById("main-form").style.background = "none";
+        }
+    });
+
+    //document.getElementById("main-form").addEventListener("submit", function (event) {
+    //    event.preventDefault();
+    //    return fetch("api/SubmitForm", {
+    //        method: "POST",
+    //        headers: {
+    //            "Content-Type": "application/json",
+    //        },
+    //        body: JSON.stringify({
+    //            value: "sentValue"
+    //        })
+    //    });
+    //});
+}
+
+function submitForm(query) {
+    var form = document.querySelector(query);
+    var formData = {
+    };
+
+    formData.Name = form.querySelector("#name").value;
+    return formData;
+}
